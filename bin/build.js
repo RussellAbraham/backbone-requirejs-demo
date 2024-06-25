4d4bdcba52e521b9c4f8dd7029914d7fe5432823
RussellAbraham <russell_a@live.com>
2024-06-24 20:39:36 -0700

    Fixed path for build.cmd

---
+++ bin/build.js
@@ -0,0 +1,13 @@
+({
+  "appDir": "src",
+  "mainConfigFile": "src/config.js",
+  "dir": "dist",
+  "generateSourceMaps": true,
+  // List the modules that will be optimized.
+  "modules": [
+      {
+          // module names are relative to baseUrl
+          "name": "config"
+      }
+  ]
+})
--- build.cmd
+++ build.cmd
@@ -4,7 +4,7 @@ set /p id="name a new path to build to : " %=%
 
 mkdir %id%
 
-node bin/r.js -o bin/build.js
+node node_modules/.bin/r.js -o build.js
 
 move dist "%id%"
 
